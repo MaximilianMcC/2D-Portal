31f004300852dcda13fdd2c843ae2ce269d7bb2e
Max <max@maximilian.co.nz>
2023-07-01 16:00:08 +1200

    Added portal buttons, and keys and mouse clicked can be detected if they are clicked once

--- InputManager.cs
+++ InputManager.cs
@@ -4,7 +4,10 @@ using SFML.Window;
 // TODO: Add support for multiple bindings for the same option. For example, arrow keys and WASD.
 class InputManager
 {
-	public static Input Keys = new Input();
+	public static Input Inputs = new Input();
+	private static Dictionary<Mouse.Button, bool> previousMouseState = new Dictionary<Mouse.Button, bool>();
+	private static Dictionary<Keyboard.Key, bool> previousKeyState = new Dictionary<Keyboard.Key, bool>();
+
 
 
 	// Check for if a key is being held down
@@ -13,34 +16,67 @@ class InputManager
 		return Keyboard.IsKeyPressed(key);
 	}
 
-	// TODO: Check for if a key has been pressed a single time
+	// Check for if a key has been pressed a single time
+	public static bool KeyPressed(Keyboard.Key key)
+	{
+		// Check for if the key was pressed in this frame, and wasn't in the previous frame
+        bool isKeyPressed = Keyboard.IsKeyPressed(key);
+        bool isPressedOnce = isKeyPressed && (!previousKeyState.ContainsKey(key) || !previousKeyState[key]);
+        
+        // Update the previous state for the next frame
+        previousKeyState[key] = isKeyPressed;
+        return isPressedOnce;
+	}
+
+	// Check for if a mouse button is being held down
+	public static bool MouseHeld(Mouse.Button button)
+	{
+		return Mouse.IsButtonPressed(button);
+	}
+
+	// Check for if a mouse button is clicked a single time
+    public static bool MouseClicked(Mouse.Button button)
+    {
+		// Check for if the button was clicked in this frame, and wasn't in the previous frame
+        bool isButtonPressed = Mouse.IsButtonPressed(button);
+        bool isClickedOnce = isButtonPressed && (!previousMouseState.ContainsKey(button) || !previousMouseState[button]);
+        
+        // Update the previous state for the next frame
+        previousMouseState[button] = isButtonPressed;
+        return isClickedOnce;
+    }
 
 }
 
 // TODO: Also include mouse and controller support
 struct Input
 {
-	// Movement keys
+	// Movement inputs
 	public Keyboard.Key MoveUp;
 	public Keyboard.Key MoveDown;
 	public Keyboard.Key MoveLeft;
 	public Keyboard.Key MoveRight;
 	public Keyboard.Key jump;
 
-	// Gameplay keys
+	// Gameplay inputs
 	public Keyboard.Key Interact;
+	public Mouse.Button FireBlue;
+	public Mouse.Button FireOrange;
+	
 
 
 	public Input()
 	{
-		// Movement keys
+		// Movement inputs
 		this.MoveUp = Keyboard.Key.Up;
 		this.MoveDown = Keyboard.Key.Up;
 		this.MoveLeft = Keyboard.Key.Left;
 		this.MoveRight = Keyboard.Key.Right;
 		this.jump = Keyboard.Key.Space;
 
-		// Gameplay keys
+		// Gameplay inputs
 		this.Interact = Keyboard.Key.E;
+		this.FireBlue = Mouse.Button.Left;
+		this.FireOrange = Mouse.Button.Right;
 	}
 }
