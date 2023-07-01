5563c39b196d5fb8f660516a985b1fd1fce84b5a
Max <max@maximilian.co.nz>
2023-07-01 15:55:39 +1200

    Began working on portal gun implementation

--- Player.cs
+++ Player.cs
@@ -4,10 +4,14 @@ using SFML.Window;
 
 class Player : GameObject
 {
+	private PortalGun portalGun;
 	private Vector2f velocity;
 	private float moveForce = 1500f;
 	private float mass = 70f; //* 70kg 
+
+	// TODO: Make these part of a map or something
 	private float friction = 0.1f;
+	private float gravity = 9.81f;
 
 
 	// Create a new player
@@ -18,27 +22,32 @@ class Player : GameObject
 
 		// Assign the position from the spawnpoint
 		Position = spawnPoint;
+
+
+
+		// Create the portal gun
+		portalGun = new PortalGun();
 	}
 
 	// Update the player
 	public override void Update()
 	{
 		Movement();
-
+		Shoot();
 	}
 
 
 
 
 
-
+	// Move the player
 	private void Movement()
 	{
 		Vector2f newPosition = Position;
 
 		// Get player movement input
-		if (InputManager.KeyHeld(InputManager.Keys.MoveLeft)) velocity.X -= moveForce * (Game.DeltaTime / mass);
-		if (InputManager.KeyHeld(InputManager.Keys.MoveRight)) velocity.X += moveForce * (Game.DeltaTime / mass);
+		if (InputManager.KeyHeld(InputManager.Inputs.MoveLeft)) velocity.X -= moveForce * (Game.DeltaTime / mass);
+		if (InputManager.KeyHeld(InputManager.Inputs.MoveRight)) velocity.X += moveForce * (Game.DeltaTime / mass);
 		
 		// Apply friction to slow down the player over time
 		velocity.X -= velocity.X * friction;
@@ -47,12 +56,13 @@ class Player : GameObject
 		// Update the position
 		newPosition += velocity;
 		Position = newPosition;
-
-		// Debug values
-		// TODO: Remove these
-		Debug.LogValue("Player X Velocity: ", velocity.X);
-		Debug.LogValue("Player X Position: ", Position.X);
 	}
 
-	
+	// Check for if the player want to shoot a portal
+	private void Shoot()
+	{
+		if (InputManager.MouseClicked(InputManager.Inputs.FireBlue)) portalGun.ShootPortal(PortalType.BLUE);
+		if (InputManager.MouseClicked(InputManager.Inputs.FireOrange)) portalGun.ShootPortal(PortalType.ORANGE);
+	}
+
 }
