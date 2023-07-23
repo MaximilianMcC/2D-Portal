5eed3b5de28f6481115663f8ec8caa4b94790501
Max <max@maximilian.co.nz>
2023-07-23 14:47:49 +1200

    Began working on making the portal collision better. kinda broke it

--- src/Game Objects/Portal.cs
+++ src/Game Objects/Portal.cs
@@ -5,7 +5,7 @@ class Portal : GameObject
 {
 	protected Player player;
 	// TODO: Don't put these in manually
-	protected int width = 6;
+	protected int width = 6; //! values lower than 6 break it idk
 	protected int height = 32;
 
 	public override void Start()
@@ -13,6 +13,7 @@ class Portal : GameObject
 		player = Game.GameObjects.OfType<Player>().First();
 	}
 
+	// Set the portals sprite
 	protected Sprite SetSprite(Sprite sprite, Direction direction)
 	{
 		// Make the sprite have the correct rotation, and direction
@@ -22,11 +23,42 @@ class Portal : GameObject
 		return sprite;
 	}
 
+	// Set the portal collision bounds
 	protected FloatRect SetBounds(Vector2f position)
 	{
 		return new FloatRect(position, new Vector2f(width, height));
 	}
+
+	// Check for portal collision, then teleport the player
+	protected void PortalCollision(Portal otherPortal)
+	{
+		// Check for if a player hits a portal
+		if (Collision() != player) return;
+
+		// Get the difference between both portals
+		float differenceX = otherPortal.Position.X - Position.X;
+		float differenceY = otherPortal.Position.Y - Position.Y;
+
+		// Get the players new position using the distances
+		float exitPositionX = player.Position.X + differenceX;
+		float exitPositionY = player.Position.Y + differenceY;
+
+		// Switch the players direction if needed
+		if (player.direction == Direction.LEFT) exitPositionX -= width;
+		else exitPositionX += width;
+
+		// Move the player to the exit position, making them go to the other portal
+		player.Position = new Vector2f(exitPositionX, exitPositionY);
+	}
 }
+
+
+
+
+
+
+
+
 
 // Blue portal
 class BluePortal : Portal
@@ -45,12 +77,8 @@ class BluePortal : Portal
 
 	public override void Update()
 	{
-		// Check for if the player has collided with the portal
-		if (Collision() == player)
-		{
-			// Send the player through the portal, and keep their velocity
-			player.Position = new Vector2f(OrangePortal.Position.X - (width * width), OrangePortal.Position.Y);
-		}
+		// Check for portal collision, then teleport the player
+		PortalCollision(OrangePortal);
 	}
 }
 
@@ -71,11 +99,7 @@ class OrangePortal : Portal
 
 	public override void Update()
 	{
-		// Check for if the player has collided with the portal
-		if (Collision() == player)
-		{
-			// Send the player through the portal, and keep their velocity
-			player.Position = new Vector2f(BluePortal.Position.X + width, BluePortal.Position.Y);
-		}
+		// Check for portal collision, then teleport the player
+		PortalCollision(BluePortal);
 	}
 }
