3e7385d404c304b60f7504949dfa649be4eddb0f
Max <max@maximilian.co.nz>
2023-07-28 20:53:40 +1200

    Can read arrays from tilemap file

--- src/Tilemap.cs
+++ src/Tilemap.cs
@@ -6,7 +6,11 @@ class Tilemap
 	public string LevelName;
 	private string[] mapFile;
 	private float tileSize;
+
+	// All the stuff that gets rendered
 	private RenderTexture background;
+
+
 
 	// Create a new tilemap
 	public Tilemap(string mapPath)
@@ -15,8 +19,8 @@ class Tilemap
 		mapFile = File.ReadAllLines(mapPath);
 
 		// Get map info
-		tileSize = GetValue<float>("tile-size");
-		LevelName = GetValue<string>("name");
+		tileSize = GetSingleValue<float>("tile-size");
+		LevelName = GetSingleValue<string>("name");
 
 		// Load the map
 		LoadMap();
@@ -25,28 +29,41 @@ class Tilemap
 
 	private void LoadMap()
 	{
-		// Create a new render texture for drawing the background
-		background = new RenderTexture(Game.Window.Size.X, Game.Window.Size.Y);
-		Texture backgroundTexture = new Texture(GetValue<string>("background"));
-
-		// Get how many tiles will be drawn on the X and Y
-		uint tilesX = (uint)Math.Ceiling(Game.Window.Size.X / tileSize);
-		uint tilesY = (uint)Math.Ceiling(Game.Window.Size.Y / tileSize);
-
-		// Loop through the entire window and create the background
-		for (int y = 0; y < tilesY; y++)
+		// Generate the background fill
 		{
-			for (int x = 0; x < tilesX; x++)
-			{
-				// Create the sprite
-				Sprite backgroundTile = new Sprite(backgroundTexture);
-				backgroundTile.Position = new Vector2f(x * tileSize, y * tileSize);
+			// Create a new render texture for drawing the background
+			background = new RenderTexture(Game.Window.Size.X, Game.Window.Size.Y);
+			Texture backgroundTexture = new Texture(GetSingleValue<string>("background-fill"));
 
-				// Add it to the background
-				background.Draw(backgroundTile);
+			// Get how many tiles will be drawn on the X and Y
+			uint tilesX = (uint)Math.Ceiling(Game.Window.Size.X / tileSize);
+			uint tilesY = (uint)Math.Ceiling(Game.Window.Size.Y / tileSize);
+
+			// Loop through the entire window and create the background
+			for (int y = 0; y < tilesY; y++)
+			{
+				for (int x = 0; x < tilesX; x++)
+				{
+					// Create the sprite
+					Sprite backgroundTile = new Sprite(backgroundTexture);
+					backgroundTile.Position = new Vector2f(x * tileSize, y * tileSize);
+
+					// Add it to the background
+					background.Draw(backgroundTile);
+				}
+			}
+			background.Display();
+
+		}
+	
+		// Generate the background layer (no collisions)
+		{
+			string[] map = GetArrayValue("background-layer");
+			foreach (var item in map)
+			{
+				Console.WriteLine(item);
 			}
 		}
-		background.Display();
 	}
 
 
@@ -59,9 +76,8 @@ class Tilemap
 
 
 
-
 	// Get a value from a map file
-	private T GetValue<T>(string key)
+	private T GetSingleValue<T>(string key)
 	{
 		key = key.ToUpper() + ": ";
 		foreach (string line in mapFile)
@@ -86,5 +102,39 @@ class Tilemap
 
 		// Nothing was found
 		return default(T);
+	}
+
+	private string[] GetArrayValue(string key)
+	{
+		List<string> array = new List<string>();
+		bool foundKey = false;
+
+		key = key.ToUpper();		
+		foreach (string line in mapFile)
+		{
+			// Check for if the correct key is found
+			if (line.StartsWith($"{key}[]:"))
+			{
+				foundKey = true;
+				continue;
+			}
+
+			// Add the value
+			if (foundKey)
+			{
+				// Add the current line/value to the array
+				array.Add(line.Replace(";", "").Trim());
+
+				// Check for if the array ended
+				if (line.EndsWith(";"))
+				{
+					foundKey = false;
+					break;
+				}
+			}
+		}
+
+		// Return the array
+		return array.ToArray();
 	}
 }
